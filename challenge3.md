d76a6e103362c8ea97ce4b495d93cc14e58c2122
folicks <axelnajera0@gmail.com>
2025-05-11 21:19:25 -0700

    challenge 3 done

---
+++ challenge3.md
@@ -0,0 +1,8 @@
+Some DOM-related stuff you may need in this challenge:
+● document.createElement(/* some HTML tag here */) - creates an HTML element
+● element.innerHTML = /* HTML goes here */ - sets the inner HTML of an element
+● element.classList.add(/* string with class name here */) - adds a class to the given
+element 
+
+
+
--- index.js
+++ index.js
@@ -29,26 +29,55 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++) {
+        const game = games[i];
 
         // create a new div element, which will become the game card
+        const gameCard = document.createElement("div");
 
 
         // add the class game-card to the list
+        gameCard.classList.add("game-card");
 
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
+        gameCard.innerHTML = `
+            <img src="${game.img}" />
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+            <p>Pledged: $${game.pledged} of $${game.goal}</p>
+            <p>Backers: ${game.backers}</p>
+        `;
 
 
         // append the game to the games-container
+        gamesContainer.appendChild(gameCard);
+
+        console.log(1);
+    }
 
 }
 
+
+function secretKey2(){
+
+    let x = 10;
+    let y =4;
+    console.log("The answer to ${x} + ${y} is ${x + y}")
+    
+
+}
+ 
+
+
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
+addGamesToPage(GAMES_JSON);
+secretKey2();
+
 
 
 /*************************************************************************************
